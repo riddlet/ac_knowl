9d8f183c1569a4ba029c58ab220fda25819e0109
Travis Riddle <riddleta@promethium.nimh.nih.gov>
2019-05-10 09:36:38 -0400

    redo initial commit

---
+++ data_exploration.R
@@ -0,0 +1,90 @@
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(reticulate)
+library(tidyr)
+
+use_virtualenv('/Users/riddleta/Desktop/misc_py3_env/')
+source_python('/Users/riddleta/Desktop/pickel_reader.py')
+
+pickle_data <- read_pickle_file("/Users/riddleta/Desktop/promethium/home/riddleta/ac_knowl/output/testdat.pkl")
+
+
+pickle_data %>%
+  select(doi, date) %>%
+  mutate(date = as_date(date)) %>%
+  mutate(year = year(date),
+         month = month(date)) %>%
+  distinct() %>%
+  group_by(year) %>%
+  summarise(n_papes = n()) %>%
+  select(year, n_papes) %>%
+  ungroup() %>%
+  ggplot(aes(x=year, y=n_papes, group=1)) + 
+  geom_line()
+
+pickle_data %>%
+  select(doi, date) %>%
+  mutate(date = as_date(date)) %>%
+  distinct() %>%
+  arrange(desc(date)) %>%
+  head(.)
+
+pickle_data %>%
+  select(discipline, date, institution, doi) %>%
+  distinct() %>%
+  mutate(date = as_date(date)) %>%
+  mutate(year = year(date)) %>%
+  group_by(year, discipline, institution) %>%
+  summarise(n_obs = n()) %>%
+  ungroup() %>%
+  group_by(year, discipline) %>%
+  mutate(total_papes = sum(n_obs)) %>%
+  mutate(prop_papes = n_obs/total_papes) %>%
+  ungroup() %>%
+  filter(year>1990, total_papes>50) -> year_disc_summaries
+
+year_disc_summaries %>%
+  group_by(discipline) %>%
+  mutate(total_disc_papes = sum(n_obs)) %>%
+  ungroup() %>%
+  select(discipline, total_disc_papes) %>%
+  distinct() -> papes_by_disc
+
+year_disc_summaries %>%
+  group_by(year, discipline) %>%
+  summarise(disc_diversity = sum(prop_papes^2)) %>%
+  ungroup() -> year_div
+
+ggplot(year_div, aes(x=year, y=disc_diversity, group=discipline)) + geom_line()
+
+year_div %>%
+  filter(discipline %in% c('Artificial intelligence', 'Bioinformatics', 'Biology', 'Ecology', 
+                           'Economics', 'Genetics', 'Machine learning', 'Physics', 'Psychology')) %>%
+  ggplot(aes(x=year, y=disc_diversity)) + geom_line() + facet_wrap(~discipline)
+
+papes_by_disc %>%
+  filter(total_disc_papes>2000) %>%
+  select(discipline) %>%
+  distinct() -> disc
+
+year_div %>%
+  filter(discipline %in% disc$discipline)  %>%
+  ggplot(aes(x=year, y=disc_diversity, group=discipline)) + geom_line() + facet_wrap(~discipline)
+
+# might be worthwhile to assess how many people are working in a field. this is a proxy for competition. maybe try to associate it with grant funding?
+
+df <- rbind(read.csv('/Users/riddleta/Desktop/promethium/home/riddleta/ac_knowl/output/bionlp_01.csv'),
+           read.csv('/Users/riddleta/Desktop/promethium/home/riddleta/ac_knowl/output/bionlp_02.csv'),
+           read.csv('/Users/riddleta/Desktop/promethium/home/riddleta/ac_knowl/output/bionlp_03.csv'),
+           read.csv('/Users/riddleta/Desktop/promethium/home/riddleta/ac_knowl/output/bionlp_04.csv'),
+           read.csv('/Users/riddleta/Desktop/promethium/home/riddleta/ac_knowl/output/bionlp_05.csv'),
+           read.csv('/Users/riddleta/Desktop/promethium/home/riddleta/ac_knowl/output/bionlp_06.csv'),
+           read.csv('/Users/riddleta/Desktop/promethium/home/riddleta/ac_knowl/output/bionlp_07.csv'))
+
+df %>%
+  select(-X, -idx) %>%
+  distinct() -> df
+
+table(df$yr)
+hist(df$yr, breaks=50)
